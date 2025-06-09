3ac7f9a1e3186469d9f3b6c5a563b72442903344
TalipTun <taliptun62@gmail.com>
2025-06-09 13:15:41 +0300

    managed to implement css variables to use with tailwindcss in my page.tsx

--- ai-interview-coach/src/app/page.tsx
+++ ai-interview-coach/src/app/page.tsx
@@ -1,9 +1,9 @@
-
+import "./globals.css";
 
 export default function Home() {
   return (
-    <main>
-      <p>test</p>
-    </main>
+    <main className="bg-background text-primary h-screen w-full text-5xl">
+      <p>Testing CSS variables directly!</p>
+  </main>
   );
 }
