e53c6ff9d900a228e16b7ea427f1cff3fd4bb320
TalipTun <taliptun62@gmail.com>
2025-06-10 21:38:19 +0300

    created the fundamental layout for the interview page, will do functionality later

--- ai-interview-coach/src/app/interview/page.tsx
+++ ai-interview-coach/src/app/interview/page.tsx
@@ -1,3 +1,13 @@
 export default function Page() {
-    return <p>This is the interview page</p>
+    return (
+    <main className="flex h-screen p-2">
+        <div className="bg-background w-1/2 h-full overflow-auto resize-x">
+
+        </div>
+        <div className="h-full w-2 bg-amber-700"></div>
+        <div className="bg-accent w-1/2 h-full flex-1">
+
+        </div>
+    </main>
+)
 }
