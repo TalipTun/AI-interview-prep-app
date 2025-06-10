398a8262582e647d8650432679a7464d6dc1136a
TalipTun <taliptun62@gmail.com>
2025-06-10 21:11:41 +0300

    created the link for begin practice button, and the interview page

--- ai-interview-coach/src/app/page.tsx
+++ ai-interview-coach/src/app/page.tsx
@@ -1,12 +1,13 @@
 import "./globals.css";
+import Link from 'next/link'
 
 export default function Home() {
   return (
-    <main className="bg-background text-primary h-screen w-full flex flex-col items-center justify-center p-6">
-      <div className="border-4 border-accent flex flex-col items-center text-center gap-y-4">
+    <main className="bg-background h-screen w-full flex flex-col items-center justify-center p-6">
+      <div className="flex flex-col items-center text-center gap-y-4">
         <p className="text-5xl text-primary font-extrabold">Master your Technical Interviews</p>
         <p className="text-secondary text-xl">Get instant, AI-powered feedback on your problem undestanding, solution code, and explanations</p>
-        <button className="bg-accent text-xl text-white flex flex-col justify-center items-center py-4 px-10 rounded-xl"> Begin Practice </button>
+        <Link href="/interview" className="bg-accent text-xl text-white flex flex-col justify-center items-center py-4 px-10 rounded-xl"> Begin Practice </Link>
       </div>
     </main>
   );
