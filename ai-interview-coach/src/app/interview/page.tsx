c59661994bceb8e7ebf37d72bc8a0ed889f5be3c
TalipTun <taliptun62@gmail.com>
2025-06-11 22:33:55 +0300

    trying to make the resizeable bar

--- ai-interview-coach/src/app/interview/page.tsx
+++ ai-interview-coach/src/app/interview/page.tsx
@@ -1,11 +1,36 @@
+'use client';
+import { useRef, useEffect, useState } from "react"
+
 export default function Page() {
+    const [leftPaneWidth, setLeftPaneWidth] = useState(100);
+    let prevX = 0;
+    let currX = 0;
+
+    function handleDrag() {
+        setLeftPaneWidth(600);
+        var diff = currX - prevX;
+        setLeftPaneWidth(100 + diff);
+    }
+
+    function handleMouseDown(event: React.MouseEvent<HTMLDivElement>) {
+        prevX = event.clientX;
+        console.log("Mouse down on:", prevX);
+    }
+
+    function handleMouseUp(event: React.MouseEvent<HTMLDivElement>) {
+        currX = event.clientX;
+        console.log("Mouse up on:", currX);
+    }
+
     return (
-    <main className="flex h-screen p-2">
-        <div className="bg-background w-1/2 h-full overflow-auto resize-x">
+    <main className="flex h-screen m-0 p-0">
+        <div style={ {width: leftPaneWidth + "px"} }  className="bg-background h-full overflow-auto resize-x">
 
         </div>
-        <div className="h-full w-2 bg-amber-700"></div>
-        <div className="bg-accent w-1/2 h-full flex-1">
+
+        <div onClick={handleDrag} onMouseDown={handleMouseDown} onMouseUp={handleMouseUp} className="h-full w-2 bg-amber-700 cursor-ew-resize"></div>
+
+        <div className="bg-accent h-full flex-1">
 
         </div>
     </main>
