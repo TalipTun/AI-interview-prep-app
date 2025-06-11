da93e14528372bffe423cc09a5f8b24da63ed231
TalipTun <taliptun62@gmail.com>
2025-06-12 01:24:36 +0300

    successfully created the main resizable bar, splitting the two sides of the screen. Made sure both of the sides have a min and a max width

--- ai-interview-coach/src/app/interview/page.tsx
+++ ai-interview-coach/src/app/interview/page.tsx
@@ -2,35 +2,55 @@
 import { useRef, useEffect, useState } from "react"
 
 export default function Page() {
-    const [leftPaneWidth, setLeftPaneWidth] = useState(100);
-    let prevX = 0;
-    let currX = 0;
+    const containerRef = useRef<HTMLDivElement>(null);
+    const [leftWidth, setLeftWidth] = useState(0);
+    const [rightWidth, setRightWidth] = useState(0);
+    const [isDragging, setIsDragging] = useState(false);
 
-    function handleDrag() {
-        setLeftPaneWidth(600);
-        var diff = currX - prevX;
-        setLeftPaneWidth(100 + diff);
+    //this code makes two sides split equally in the beginning, on mount.
+    useEffect(() => {
+        if (containerRef.current) {
+            const totalWidth = containerRef.current.offsetWidth;
+            setLeftWidth(totalWidth / 2);
+            setRightWidth(totalWidth / 2);
+        }
+    }, [])
+
+    function mouseDown() {
+        setIsDragging(true);
+        document.addEventListener("mousemove", mouseMove);
+        document.addEventListener("mouseup", mouseUp);
     }
 
-    function handleMouseDown(event: React.MouseEvent<HTMLDivElement>) {
-        prevX = event.clientX;
-        console.log("Mouse down on:", prevX);
+    const mouseMove = (e : MouseEvent) => {
+        if (containerRef.current == null) return;
+
+        const containerRect = containerRef.current.getBoundingClientRect();
+        const xDiff = e.clientX - containerRect.left
+        const min = 200;
+
+        const newLeftWidth = Math.max(min, Math.min(xDiff, containerRect.right - min));
+        const newRightWidth = containerRect.width - newLeftWidth - 2;
+
+        setLeftWidth(newLeftWidth);
+        setRightWidth(newRightWidth);
     }
 
-    function handleMouseUp(event: React.MouseEvent<HTMLDivElement>) {
-        currX = event.clientX;
-        console.log("Mouse up on:", currX);
+    function mouseUp() {
+        setIsDragging(false);
+        document.removeEventListener("mousemove", mouseMove);
+        document.removeEventListener("mouseup", mouseUp);
     }
 
     return (
-    <main className="flex h-screen m-0 p-0">
-        <div style={ {width: leftPaneWidth + "px"} }  className="bg-background h-full overflow-auto resize-x">
+    <main ref={containerRef} className="flex w-screen h-screen border-4 border-e-accent">
+        <div style={ {width : leftWidth + "px" }} className="bg-background h-full w-50">
 
         </div>
 
-        <div onClick={handleDrag} onMouseDown={handleMouseDown} onMouseUp={handleMouseUp} className="h-full w-2 bg-amber-700 cursor-ew-resize"></div>
+        <div onMouseDown={mouseDown} className="h-full w-2 bg-amber-700 cursor-ew-resize"></div>
 
-        <div className="bg-accent h-full flex-1">
+        <div style={ {width : rightWidth + "px" }} className="bg-accent h-full flex-1">
 
         </div>
     </main>
